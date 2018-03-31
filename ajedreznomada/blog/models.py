5610e8d26802a04000df76a8cae8047f1ed04de1
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-03-31 00:52:39 -0600

    add blog and category model

--- ajedreznomada/blog/models.py
+++ ajedreznomada/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Blog(models.Model):
+    ''' Blog model '''
+
+    # Fk fields
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='blogs', blank=True, null=True)
+    category = models.ForeignKey('blog.Category', related_name='blogs', blank=True, null=True)
+
+
+    title = models.CharField('Titulo', max_length=255, default='')
+    description = models.CharField('Descripción', max_length=255, blank=True, default='')
+    content = models.TextField('Contenido', blank=True, default='')
+
+    # Util datatimes
+    created_at = models.DateTimeField("Fecha de creación", auto_now_add=True)
+    last_modified = models.DateTimeField("Última modificación", auto_now=True)
+
+    def __str__(self):
+        ''' Define str of model '''
+        return self.title
+
+
+
+class Category(models.Model):
+    ''' Category Model '''
+    name = models.CharField('Nombre', max_length=255, blank=True, default='')
+    description = models.CharField('Descripción', max_length=255, blank=True, default='')
+
+    def __str__(self):
+        ''' Define str of model '''
+        return self.name
