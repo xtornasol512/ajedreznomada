30d20654b8ee1915d8b61968ad2a6ae5848abf0d
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-04-01 16:56:04 -0500

    add emotion to title

--- ajedreznomada/static/js/main.js
+++ ajedreznomada/static/js/main.js
@@ -6,3 +6,21 @@ $('#welcome-button').click(function() {
   var name = $('#welcome').val()
   window.location.href = "/welcome/" + name;
 });
+
+$( document ).ready(function() {
+  var burst = new mojs.Burst({
+    top: 20,
+    left: 70,
+    parent: main_title,
+    radius: {15 : 50},
+    children : {
+      fill : ['deeppink', 'cyan', 'orange']
+    }
+  });
+
+  $('#main_title').click(function(e){
+    burst
+      .replay()
+  });
+});
+
