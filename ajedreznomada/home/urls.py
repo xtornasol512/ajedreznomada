1a1e8c87f40f3920f6091a4733762c1f3f728cbf
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-04-02 13:29:22 -0500

    accept email too

--- ajedreznomada/home/urls.py
+++ ajedreznomada/home/urls.py
@@ -5,6 +5,7 @@ from .views import home, logout_page, welcome
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^welcome/$', welcome, name='welcome_alone'),
+    url(r'^welcome/(?P<name>[\w.@+-]+)/$', welcome, name='welcome_email'),
     url(r'^welcome/(?P<name>[A-Za-z]*)/$', welcome, name='welcome'),
     url(r'^logout/$', logout_page, name='logout_page'),
 ]
